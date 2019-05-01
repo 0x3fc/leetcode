3a580c6b3dbd2202a39123b38b9f364aa8af018f
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-04-30 17:29:07 -0700

    Reverse integer

---
+++ 7.reverse-integer.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=7 lang=java
+ *
+ * [7] Reverse Integer
+ */
+class Solution {
+    public int reverse(int x) {
+        boolean isNeg = x < 0;
+
+        if (isNeg) {
+            x = -x;
+        }
+
+        long sum = 0;
+        int b;
+
+        while (x > 0) {
+            b = x % 10;
+            x = x / 10;
+
+            sum = sum * 10 + b;
+        }
+
+        if (isNeg) {
+            sum = -sum;
+        }
+
+        boolean overflowed = sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE;
+
+        return overflowed ? 0 : (int) sum;
+    }
+}
