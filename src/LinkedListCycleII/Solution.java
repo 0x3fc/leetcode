b205d2a44a9a682328b832a74d2bc126d78e5ba3
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-05-03 23:43:48 -0700

    Linked list cycle II

---
+++ src/LinkedListCycleII/Solution.java
@@ -0,0 +1,30 @@
+package LinkedListCycleII;
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        do {
+            if (fast.next == null || fast.next.next == null) {
+                return null;
+            }
+
+            fast = fast.next.next;
+            slow = slow.next;
+        } while (fast != slow);
+
+        slow = head;
+
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return fast;
+    }
+}
