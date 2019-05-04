1b89fef99aaf74fa084faa358f7f650be85aa628
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-05-03 23:47:48 -0700

    Linked list cycle

---
+++ src/LinkedListCycle/ListNode.java
@@ -0,0 +1,11 @@
+package LinkedListCycle;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ src/LinkedListCycle/Solution.java
@@ -0,0 +1,24 @@
+package LinkedListCycle;
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        do {
+            if (fast.next == null || fast.next.next == null) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+        } while (slow != fast);
+
+        return true;
+    }
+}
