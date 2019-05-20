58dd72dbf519d3d5e610143c602fc6148c8f17ee
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-05-20 15:34:00 -0700

    partition labels

---
+++ src/PartitionLabels/Solution.java
@@ -0,0 +1,28 @@
+package PartitionLabels;
+
+import java.util.List;
+
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];
+
+        for (int i = 0; i < S.length(); i++) {
+            last[S.charAt(i) - 'a'] = i;
+        }
+
+        int lastIndex = 0;
+        int start = -1;
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < S.length(); i++) {
+            lastIndex = Math.max(lastIndex, last[S.charAt(i) - 'a']);
+
+            if (lastIndex == i) {
+                res.add(lastIndex - start);
+                start = i;
+            }
+        }
+
+        return res;
+    }
+}
