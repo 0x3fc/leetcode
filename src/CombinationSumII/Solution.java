f83e1a22b9ddf026cc2dea5923e823c2d025e098
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-06-05 18:28:46 -0700

    Combination sum II

---
+++ src/CombinationSumII/Solution.java
@@ -0,0 +1,37 @@
+package CombinationSumII;
+
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Arrays.sort(candidates);
+
+        backtrack(res, new ArrayList<>(), candidates, target, 0);
+
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, List<Integer> comb, int[] candidates, int target, int start) {
+        if (target == 0) {
+            res.add(new ArrayList<>(comb));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            int candidate = candidates[i];
+
+            if (candidate > target) {
+                break;
+            }
+
+            // Skip duplicates
+            if (i > start && candidate == candidates[i - 1]) {
+                continue;
+            }
+
+            comb.add(candidate);
+            backtrack(res, comb, candidates, target - candidate, i + 1);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
