00bdc727feff169fc046a0dd54483d3f504a3d44
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-06-07 16:39:09 -0700

    Contains duplicate

---
+++ src/ContainsDuplicate/Solution.java
@@ -0,0 +1,29 @@
+package ContainsDuplicate;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+
+        for (int num : nums) {
+            if (s.contains(num)) {
+                return true;
+            }
+
+            s.add(num);
+        }
+
+        return false;
+    }
+
+    public boolean containsDuplicate2(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
