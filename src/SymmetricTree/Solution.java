41dea195b98018d21b4d770c6904ce4429f76a28
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-07-05 10:16:01 -0700

    Symmetric tree

---
+++ src/SymmetricTree/Solution.java
@@ -0,0 +1,20 @@
+package SymmetricTree;
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetricHelper(root, root);
+    }
+
+    public boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        return left.val == right.val && isSymmetricHelper(left.right, right.left)
+                && isSymmetricHelper(left.left, right.right);
+    }
+}
