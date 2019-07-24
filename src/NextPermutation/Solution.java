d9897edf23e6810b866fa04688e2d54343326496
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-07-24 10:12:22 -0700

    Next permutation

---
+++ src/NextPermutation/Solution.java
@@ -0,0 +1,40 @@
+package NextPermutation;
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        // Finding the first decreasing element from the end
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+
+            // Find the number that just larger then the decreasing element
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
