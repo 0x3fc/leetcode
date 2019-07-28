b6c7e7cc0bf09374b1b07c40e8e9d6dfdfc31ff8
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-07-28 10:36:25 -0700

    Binary tree inorder traversal

---
+++ src/BinaryTreeInorderTraversal/Solution.java
@@ -0,0 +1,24 @@
+package BinaryTreeInorderTraversal;
+
+import java.util.*;
+import Common.TreeNode;
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        inorder(root, res);
+
+        return res;
+    }
+
+    private void inorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left, res);
+        res.add(node.val);
+        inorder(node.right, res);
+    }
+}
