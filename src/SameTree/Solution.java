7f80fe923c3abf1193c5b9ad1e92d93a6a7996aa
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-09-05 21:35:57 -0700

    Same tree

---
+++ src/SameTree/Solution.java
@@ -0,0 +1,13 @@
+package SameTree;
+
+import Common.TreeNode;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
