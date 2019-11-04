cde2cd1076cb7542503a6d7f204ca1e3cd9311d1
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-11-03 18:54:21 -0800

    Search in rotated sorted array

--- src/SearchInRotatedSortedArray/Solution.java
+++ src/SearchInRotatedSortedArray/Solution.java
@@ -5,29 +5,30 @@ class Solution {
         int i = 0;
         int j = nums.length - 1;
 
-        int mid;
-
         while (i <= j) {
-            mid = (i + j) / 2;
-
+            int mid = (i + j) / 2;
+            
             if (nums[mid] == target) {
                 return mid;
             }
+            
+            boolean midAtPivotLeft = nums[0] <= nums[mid];
+            boolean targetAtPivotLeft = nums[0] <= target;
+            boolean isSameSide = targetAtPivotLeft == midAtPivotLeft;
+            
+            int midValue = isSameSide
+                ? nums[mid]
+                : targetAtPivotLeft
+                    ? Integer.MAX_VALUE
+                    : Integer.MIN_VALUE;
 
-            boolean isPivotInLeft = nums[0] > nums[mid];
-            boolean isTargetInLeft = target >= nums[0];
-
-            double modifiedMid = isPivotInLeft == isTargetInLeft
-                    ? isTargetInLeft ? Double.POSITIVE_INFINITY : Double.NEGATIVE_INFINITY
-                    : nums[mid];
-
-            if (modifiedMid < target) {
+            if (midValue < target) {
                 i = mid + 1;
             } else {
                 j = mid - 1;
             }
         }
-
+        
         return -1;
     }
 }
