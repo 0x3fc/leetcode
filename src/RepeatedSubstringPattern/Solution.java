ea21909f9a8485d1b26972eb7cfb34631e204417
Senhung <alexwongsenhung@gmail.com>
2020-09-05 12:37:20 +0000

    Repeated substring pattern

---
+++ src/RepeatedSubstringPattern/Solution.java
@@ -0,0 +1,7 @@
+package RepeatedSubstringPattern;
+
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        return (s + s).substring(1, s.length() * 2 - 1).contains(s);
+    }
+}
