8dc6d4f4a713be615ffdfccdc99220ac1bad324e
Senhung <alexwongsenhung@gmail.com>
2020-09-30 12:09:12 +0000

    First missing positive

--- src/FirstMissingPositive/Solution.java
+++ src/FirstMissingPositive/Solution.java
@@ -2,33 +2,22 @@ package FirstMissingPositive;
 
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        if (nums.length < 1) {
-            return 1;
-        }
-
-        int i = 0;
-
-        while (i < nums.length) {
-            if (nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[nums[i] - 1]) {
-                swap(nums, i, nums[i] - 1);
-                continue;
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] < nums.length
+            // Destiny is not the correct number
+                    && nums[nums[i] - 1] != nums[i]) {
+                int index = nums[i] - 1;
+                int tmp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = tmp;
             }
-
-            i++;
         }
 
-        i = 0;
-
-        while (i < nums.length && nums[i] == i + 1) {
-            i++;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
         }
 
-        return i + 1;
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+        return nums.length + 1;
     }
 }
