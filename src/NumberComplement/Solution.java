4440dc2b0457ded034982bcbc9d15d2034b27454
Senhung <alexwongsenhung@gmail.com>
2020-10-05 11:39:33 +0000

    Number complement

---
+++ src/NumberComplement/Solution.java
@@ -0,0 +1,10 @@
+package NumberComplement;
+
+class Solution {
+    public int findComplement(int num) {
+        int x = 1;
+        while (x < num)
+            x = (x << 1) | 1;
+        return x ^ num;
+    }
+}
