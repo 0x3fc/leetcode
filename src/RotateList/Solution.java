bf553d7bae00f623e8681c1b10bdf9d62458801f
Senhung <alexwongsenhung@gmail.com>
2020-10-07 10:35:55 +0000

    Rotate list

--- src/RotateList/Solution.java
+++ src/RotateList/Solution.java
@@ -29,4 +29,53 @@ class Solution {
 
         return dummy.next;
     }
+
+    public ListNode rotateRightWithoutDummy(ListNode head, int k) {
+        if (head == null || k == 0)
+            return head;
+
+        ListNode last = head;
+        int count = 1;
+        while (last.next != null) {
+            last = last.next;
+            count++;
+        }
+
+        ListNode pivot = head;
+        int pos = count - k % count;
+        while (pos > 1) {
+            pivot = pivot.next;
+            pos--;
+        }
+
+        last.next = head;
+        head = pivot.next;
+        pivot.next = null;
+
+        return head;
+    }
+
+    public ListNode rotateRightFormCircle(ListNode head, int k) {
+        if (head == null || k == 0)
+            return head;
+
+        int count = 1;
+        ListNode iter = head;
+        while (iter.next != null) {
+            iter = iter.next;
+            count++;
+        }
+
+        iter.next = head;
+
+        int pos = count - k % count;
+        while (pos > 0) {
+            iter = iter.next;
+            pos--;
+        }
+
+        head = iter.next;
+        iter.next = null;
+        return head;
+    }
 }
