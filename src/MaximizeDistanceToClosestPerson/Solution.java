8c4e856522a699fe17d5d3bd373b4b793b77cb5a
Senhung <alexwongsenhung@gmail.com>
2020-10-29 11:13:31 +0000

    Maximize distance to closest person

--- src/MaximizeDistanceToClosestPerson/Solution.java
+++ src/MaximizeDistanceToClosestPerson/Solution.java
@@ -23,4 +23,28 @@ class Solution {
 
         return Math.max(count, max);
     }
+
+    public int maxDistToClosestTwoPointer(int[] seats) {
+        int i = 0;
+        int j = seats.length - 1;
+
+        while (seats[i] == 0)
+            i++;
+        while (seats[j] == 0)
+            j--;
+        int max = Math.max(i, seats.length - j - 1);
+
+        while (i < j) {
+            int empty = 0;
+            while (i < j && seats[i] == 1)
+                i++;
+            while (i < j && seats[i] == 0) {
+                i++;
+                empty++;
+            }
+            max = Math.max(max, (empty + 1) / 2);
+        }
+
+        return max;
+    }
 }
