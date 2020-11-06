d2e582dfa4b702a8f2ce85fbbb2873b4b5834e1e
Senhung Wong <alexwongsenhung@gmail.com>
2020-11-06 03:27:30 +0000

    Contains duplicate II

---
+++ src/ContainsDuplicateII/Solution.java
@@ -0,0 +1,19 @@
+package ContainsDuplicateII;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> s = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k)
+                s.remove(nums[i - k - 1]);
+            if (!s.add(nums[i]))
+                return true;
+        }
+
+        return false;
+    }
+}
